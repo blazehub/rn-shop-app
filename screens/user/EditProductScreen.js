5b6b7d08c19a00df2039155d48cad915b68d1c6d
blazehub <blaze.avinash@gmail.com>
2020-03-19 12:30:14 +0530

    submit action for product

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { ScrollView, TextInput, StyleSheet, View, Text, Platform } from 'react-native';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
@@ -17,6 +17,14 @@ const EditProductScreen = (props) => {
     const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '');
     const [price, setPrice] = useState('');
     const [description, setDescription] = useState(editedProduct ? editedProduct.description : '');
+
+    const submitHandler = useCallback(() => {
+        console.log('Submitting..');
+    }, []);
+
+    useEffect(() => {
+        props.navigation.setParams({ submit: submitHandler });
+    }, [submitHandler]);
 
     return (
         <ScrollView>
@@ -45,13 +53,12 @@ const EditProductScreen = (props) => {
 }
 
 EditProductScreen.navigationOptions = navData => {
+    const submitFn = navData.navigation.getParam('submit');
     return {
         headerTitle: navData.navigation.getParam('productId') ? 'Edit Product' : 'Add Product',
         headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
             <Item title='Save' iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
-                onPress={() => {
-
-                }}></Item>
+                onPress={submitFn}></Item>
         </HeaderButtons>
     }
 }
