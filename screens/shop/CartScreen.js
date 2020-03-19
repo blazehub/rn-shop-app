3f9cc732f473e2ac1644a961cc422010af56f47d
blazehub <blaze.avinash@gmail.com>
2020-03-19 13:23:57 +0530

    app improvements

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -7,6 +7,7 @@ import Colors from '../../constants/Colors';
 
 import * as cartActions from '../../store/actions/cart';
 import * as orderActions from '../../store/actions/order';
+import Card from '../../components/UI/Card';
 
 const CartScreen = (props) => {
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -28,7 +29,7 @@ const CartScreen = (props) => {
 
     return (
         <View style={styles.screen}>
-            <View style={styles.summary}>
+            <Card style={styles.summary}>
                 <Text style={styles.summaryText}>
                     Total: <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
                 </Text>
@@ -36,7 +37,7 @@ const CartScreen = (props) => {
                     onPress={() => {
                         dispatch(orderActions.addOrder(cartItems, cartTotalAmount));
                     }}></Button>
-            </View>
+            </Card>
             <FlatList data={cartItems}
                 keyExtractor={(item) => item.productId}
                 renderItem={itemData => <CardItem
@@ -65,14 +66,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         marginBottom: 20,
-        padding: 10,
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: 'white',
+        padding: 10
     },
     summaryText: {
         fontFamily: 'open-sans-bold',
